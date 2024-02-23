28a2f27f7917b06959cd1eccca499c722a032840
KhilanVitthani <khilanvithani0080@gmail.com>
2024-02-23 15:08:54 +0530

    initial commit.

---
+++ android/app/src/main/kotlin/com/example/toughest_new/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.toughest_new
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:toughest_new/ui/home.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(fontFamily: 'Josefin Sans'),
+      home: Home(),
+    );
+  }
+}
